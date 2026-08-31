60d1f6f82b9b0ccf75c207887e572665791af367
Yash Kadam <yashkadam6007@gmail.com>
2026-08-31 13:37:54 +0530

    Fix database configuration for deployment

--- app/__init__.py
+++ app/__init__.py
@@ -23,7 +23,7 @@ def create_app():
         database=os.getenv("DB_NAME")
     )
 
-    
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_url
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
